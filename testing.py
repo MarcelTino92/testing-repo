e0fd5c9e4da3dc78e1ace81ff00225db89cd4c9b
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-01 23:30:55 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 data = {"username": 1, "password": 2,"question":3}
-url = f"https://programmingtraining.sawtoothsoftware.com/CTDEMOCHI_V1/cgi-bin/ciwweb.pl?studyname=CTDEMOCHI_V1&sys_skipto=D1PostText&transaction_id=5&CT-1&hid_pagenum=1&hid_link=1&hid_javascript=1&hid_screenwidth=1488"
+url = f"https://programmingtraining.sawtoothsoftware.com/CTDEMOCHI_V1/cgi-bin/ciwweb.pl?studyname=CTDEMOCHI_V1&sys_skipto=D1PostText&transaction_id=5&CT-1&hid_pagenum=1&hid_link=1&hid_javascript=1&hid_screenwidth=1488/embed=True"
 response = requests.get(url, params=data)
 
 res_id= f"{st.query_params.respondent}"
