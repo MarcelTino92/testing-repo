c47abce8fe5bb5dee94c99d6753c3b16c063d430
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-02 19:14:47 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,10 +6,11 @@ import streamlit.components.v1 as components
 # Create the HTML for CSP and iframe
 html_code = f"""
     <<script>
+        <iframe is="x-frame-bypass" src="https://example.org><iframe>
         // Define the URL to open
-        var urlToOpen = "https://copilot.microsoft.com";
+        #var urlToOpen = "https://copilot.microsoft.com";
         // Open a new window with the specified URL
-        window.open(urlToOpen, "_self");
+        #window.open(urlToOpen, "_self");
     </script>
 """
 
