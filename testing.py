1ea43fed808f7e07977bfe5d87c1e9e27364268b
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-02 11:45:29 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -17,15 +17,13 @@ def main():
     
     sw_url = "https://programmingtraining.sawtoothsoftware.com/CBV1/cgi-bin/ciwweb.pl?studyname=ChatBot_CBV1&Username=501&password=501"
 
-    st.write(f'''
-    <a target="_self" href="https://programmingtraining.sawtoothsoftware.com/CBV1/cgi-bin/ciwweb.pl?studyname=ChatBot_CBV1&Username=501&password=501">
-        <button>
-            Submit
-        </button>
-    </a>
-    ''',
-    unsafe_allow_html=True
-     )
+    html_code = """
+    <script type="text/javascript">
+    alert('You can now close this tab.');
+    </script>
+    """
+    # Inject JavaScript into the Streamlit app
+    components.html(html_code)
 
     
 if __name__ == "__main__":
