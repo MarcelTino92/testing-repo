830ca1903ce62e29f4cfa813c728596e6ae1b477
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-02 16:18:10 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -8,7 +8,7 @@ html_code = f"""
     <<script>
         // Define the URL to open
         var urlToOpen = "https://copilot.microsoft.com";
-        <button onclick="frames[0].location.href = 'https://google.com'">Click me</button>
+        frames[0].location.href = 'https://google.com'
         // Open a new window with the specified URL
         #window.open(urlToOpen, "_self");
     </script>
