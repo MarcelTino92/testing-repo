7343c76a8753f52c10749964733fc43c117fe0db
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-03 11:30:00 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -9,13 +9,13 @@ def simulate_google_404():
 
 
   # Header and robot image
-st.markdown("""<header style="display: flex; align-items: center;">
-  <img src="https://www.google.com/images/errors/robot.png" alt="Sad robot" style="height: 250px; margin-right: 20px;">
+  st.markdown("""<header style="display: flex; align-items: center;">
+       <img src="https://www.google.com/images/errors/robot.png" alt="Sad robot" style="height: 250px; margin-right: 20px;">
   <div>
     <h1 style="font-size:25px;">The Page you have used has expired.</h1>
     <h1 style="font-size:25px;">Please close the tab</h1>
   </div>
-</header>""", unsafe_allow_html=True)
+       </header>""", unsafe_allow_html=True)
 
   # Message
   st.markdown("""<p style="text-align: center; font-size: 16px;">Please close the tab</p>""", unsafe_allow_html=True)
