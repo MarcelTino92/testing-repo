1548abf1d8c4954f6a11a2d1baee2447439e10ff
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 14:46:38 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,7 +2,6 @@ import streamlit as st
 
 import streamlit.components.v1 as components
 import streamlit as st
-url=f"https://www.google.com/search?igu=1&ei=&q="
+url=f"https://programmingtraining.sawtoothsoftware.com/CTDEMOCHI_V1/cgi-bin/ciwweb.pl?studyname=CTDEMOCHI_V1&sys_skipto=D1PostText&transaction_id=5&CT-1&hid_pagenum=1&hid_link=1&hid_javascript=1&hid_screenwidth=1488&username=893&password=893"
 #search = st.text_input("What do you want to search for?")
-components.iframe(url, height=1200)
 
