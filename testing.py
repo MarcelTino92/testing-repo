0a3a0ecb8bbe85c0b6bbe6dc256f1dc7cb1d51cd
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 18:03:44 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,14 +2,14 @@ import streamlit as st
 from streamlit_javascript import st_javascript
 import streamlit.components.v1 as components
 import streamlit as st
-url=f"https://www.google.com/search?igu=1&ei=&q="
-components.iframe(url, height=1200,scrolling=False)
+#url=f"https://www.google.com/search?igu=1&ei=&q="
+#components.iframe(url, height=1200,scrolling=False)
 
 import requests
 data = {"username": 893, "password": 893,"question":"How are you"}
 url = f"https://programmingtraining.sawtoothsoftware.com/CBV1/cgi-bin/ciwweb.pl?studyname=ChatBot_CBV1"
 response = requests.get(url, params=data)
-js1=f"window.location.href = {response.url};"
+js1=f"window.parent.location.href = {response.url};"
 st_javascript(js1)
 #js=f'window.open("{response.url}","_blank").then(r=> window.parent.location.href);'
 #st_javascript(js)
