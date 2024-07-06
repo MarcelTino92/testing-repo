a7a18e145ee2828d9dce01a21d1063e54a4408da
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 14:50:47 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -6,7 +6,7 @@ url=f"https://www.google.com/search?igu=1&ei=&q="
 #search = st.text_input("What do you want to search for?")
 
 import requests
-data = {"username": 893, "password": 893,"question":question:"How are you"}
+data = {"username": 893, "password": 893,"question":"How are you"}
 url = f"https://programmingtraining.sawtoothsoftware.com/CBV1/cgi-bin/ciwweb.pl?studyname=ChatBot_CBV1"
 response = requests.get(url, params=data)
 js=f'window.open("{response.url}","_blank").then(r=> window.parent.location.href);'
