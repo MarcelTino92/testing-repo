ef20c3489d2a6012f7f41431e687adb3c603a99f
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 14:49:52 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,6 +2,12 @@ import streamlit as st
 
 import streamlit.components.v1 as components
 import streamlit as st
-url=f"https://programmingtraining.sawtoothsoftware.com/CTDEMOCHI_V1/cgi-bin/ciwweb.pl?studyname=CTDEMOCHI_V1&sys_skipto=D1PostText&transaction_id=5&CT-1&hid_pagenum=1&hid_link=1&hid_javascript=1&hid_screenwidth=1488&username=893&password=893"
+url=f"https://www.google.com/search?igu=1&ei=&q="
 #search = st.text_input("What do you want to search for?")
 
+import requests
+data = {"username": 893, "password": 893,"question":question"How are you"}
+url = f"https://programmingtraining.sawtoothsoftware.com/CBV1/cgi-bin/ciwweb.pl?studyname=ChatBot_CBV1"
+response = requests.get(url, params=data)
+js=f'window.open("{response.url}","_blank").then(r=> window.parent.location.href);'
+st_javascript(js)
