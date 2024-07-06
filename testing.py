a7c3ec09d19c7c886bbe5af79ed87770141beb9a
MarcelTino92 <162463171+MarcelTino92@users.noreply.github.com>
2024-07-06 14:56:34 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -4,7 +4,7 @@ import streamlit.components.v1 as components
 import streamlit as st
 url=f"https://www.google.com/search?igu=1&ei=&q="
 #search = st.text_input("What do you want to search for?")
-components.iframe(url, height=1200)
+components.iframe(url, height=1200,scrolling=False)
 
 import requests
 data = {"username": 893, "password": 893,"question":"How are you"}
